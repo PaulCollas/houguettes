4217bd9c0c183c2bcef14b466d650933df694ec0
Paul Collas <paulcollas446@gmail.com>
2022-05-05 13:00:11 +0200

    Initialisation of all files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+    './index.html',],
+  theme: {
+    extend: {
+      colors: {
+        'blue': '#163E4B'
+      },
+      fontFamily: {
+        'Acumin': ["Acumin Variable Concept"],
+        'Tahu': ["Tahu"],
+        'Roboto': ["'Space Mono', monospace"],
+      }
+    },
+  },
+  plugins: [],
+}
