c097c816b7d3df14e4ce9b74e70338ffd13882ee
Robitch <poiron.robin@gmail.com>
2022-05-06 12:53:29 +0200

    Bannière + Nos locations (sans le carrousel)

--- src/script.js
+++ src/script.js
@@ -9,3 +9,44 @@ burger.addEventListener('click', function () {
     menuBurger.classList.toggle('max-h-96');
     menuBurger.classList.toggle('p-7');
 })
+
+
+/* ANIMATION CARROUSEL */
+
+let imagevisible = 0;
+let mesimages = document.querySelectorAll("img.hidden");
+let dots = document.querySelectorAll("span.opacity-50");
+const avant = document.querySelector(".avant");
+const apres = document.querySelector(".apres");
+
+mesimages[imagevisible].style.display = "block";
+dots[imagevisible].style.opacity = "1";
+
+function imagePrecedente() {
+    console.log("Yessir");
+    mesimages[imagevisible].style.display = "none";
+    dots[imagevisible].style.opacity = "0.5";
+    imagevisible -= 1;
+    if (imagevisible === -1) {
+        imagevisible = mesimages.length - 1;
+    }
+
+    mesimages[imagevisible].style.display = "block";
+    dots[imagevisible].style.opacity = "1";
+}
+
+function imageProchaine() {
+    mesimages[imagevisible].style.display = "none";
+    dots[imagevisible].style.opacity = "0.5";
+
+    imagevisible += 1;
+    if (imagevisible === mesimages.length) {
+        imagevisible = 0;
+    }
+
+    mesimages[imagevisible].style.display = "block";
+    dots[imagevisible].style.opacity = "1";
+}
+
+apres.addEventListener("click", imageProchaine);
+avant.addEventListener("click", imagePrecedente);
